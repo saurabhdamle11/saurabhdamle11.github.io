a2af0295caf7d4ab7d15af94c6999acd03ce315d
saurabhdamle11 <saurabhdamle8@gmail.com>
2025-01-28 18:43:30 -0500

    fixed github landing page serve issue

--- app.js
+++ app.js
@@ -7,31 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
-// LeetCode API Proxy Route
-app.get('/api/leetcode/:username', async (req, res) => {
-  const { username } = req.params;
-
-  try {
-    const fetch = (await import('node-fetch')).default; // Dynamic import
-    const response = await fetch('http://localhost:3000/leetcode', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username }),
-    });
-
-    if (!response.ok) {
-      throw new Error('Failed to fetch LeetCode rating');
-    }
-
-    const data = await response.json();
-    res.json(data);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).json({ error: 'Unable to fetch LeetCode rating' });
-  }
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
